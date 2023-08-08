25979dc84fa9b7e771a7d7f0b75d4a3d3e159899
rojashd <135187158+HectorRojas3@users.noreply.github.com>
2023-08-07 21:41:44 -0600

    js proyecto integrador: lista de contactos

--- app.js
+++ app.js
@@ -6,30 +6,67 @@ cada contacto debe contener el nombre y apellido como una sola cadena de caracte
 5. Publica tu código en Github, en tu repositorio contact-list crea un branch llamado project-1 y compártelo con nosotros. 👍🏼⬇️ */
 
 // step 1
-let listaDeContactos = ["Lidia Dole", "Jonata Rojas", "Hada Rodriguez", "Salvador Ramos", "Pamela Ramos", "Axel Ramos",];
+let listaDeContactos = ["Lidia Dole", "Jonatan Rojas", "Hada Rodriguez", "Salvador Ramos", "Pamela Ramos", "Axel Ramos",];
 
 // step 2
-function nuevoContacto (listaDeContactos, newElement ) {
+function nuevoContacto (listaDeContactos, newElement) {
     listaDeContactos.push(newElement);
 }
 
-nuevoContacto(listaDeContactos, "Hector Rojas");
+//nuevoContacto(listaDeContactos, "Hector Rojas");
 
 // step 3
 function borrarContacto (listaDeContactos) {
     if (listaDeContactos.length === 0) {
         console.log("no hay contacto para borrar")
-    }
-
+    } else {
     const indiceAleatorio = Math.floor(Math.random() * listaDeContactos.length);
     listaDeContactos.splice(indiceAleatorio, 1)
+    }
 }
 
-borrarContacto(listaDeContactos);
+
+
+//borrarContacto(listaDeContactos);
 
 // step 4
 function mostrarEnConsola (listaDeContactos) {
-    console.log("Lista de contactos actualizada", listaDeContactos.join(', '));
+    console.log("Lista de Contactos Actualizada:");
+    listaDeContactos.forEach(contacto => {
+        console.log(`ID: ${contacto.identidad}`);
+        console.log(`Nombres: ${contacto.nombres}`);
+        console.log(`Apellidos: ${contacto.apellidos}`);
+        console.log(`Telefono: ${contacto.telefono}`);
+        if (contacto.ubicaciones) {
+            console.log(`Ubicaciones: Ciudad: ${contacto.ubicaciones.ciudad}, Direccion: ${contacto.ubicaciones.direccion}`);
+        }
+        console.log("------------------");
+    });
 }
 
+//mostrarEnConsola(listaDeContactos);
+
+
+// Proyecto integrador 2. 
+/* 1. Ajustar las funciones de crear y eliminar un contacto para que puedan almacenar 
+la siguiente info como objetos:
+1. id
+2. apellidos
+3. telefono
+4. ubicaciones 
+    - ciudad
+    - direccion */
+
+let contacto1 = {
+    ID: 1804199300160,
+    Nombres: "Hector David",
+    Apellidos: "Rojas Rodriguez",
+    Telefono: "89027851",
+    Ubicacion: {
+        Ciudad: "El Progreso",
+        Direccion: "Palermo"
+    }
+};
+
+nuevoContacto(listaDeContactos, contacto1);
 mostrarEnConsola(listaDeContactos);
